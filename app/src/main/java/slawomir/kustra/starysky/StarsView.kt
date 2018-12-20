f0e27ffe3278374e7147ce44ad71c297de8dd5ee
skustra <kustraslawomir@gmail.com>
2018-12-20 20:28:32 +0100

    draw resized bitmap (random size)

--- app/src/main/java/slawomir/kustra/starysky/StarsView.kt
+++ app/src/main/java/slawomir/kustra/starysky/StarsView.kt
@@ -4,6 +4,14 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.widget.FrameLayout
+import slawomir.kustra.starysky.utils.getStarSize
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.DisplayMetrics
+
+
+
+
 
 class StarsView : FrameLayout {
 
@@ -29,11 +37,16 @@ class StarsView : FrameLayout {
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        canvas.drawBitmap(starIcon, 0f, 0f, paint)
+        canvas.drawBitmap(starIcon, 50f,50f,null)
     }
 
     private fun init(context: Context) {
-        starIcon = BitmapFactory.decodeResource(context.resources, R.drawable.star)
+        val screenWidth = context.resources.displayMetrics.widthPixels
+        val starSize = getStarSize(screenWidth)
+        println("screenWidth: $screenWidth, starSize: $starSize")
+
+        starIcon = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(resources, R.drawable.star), starSize, starSize, false)
+
         starDestination = Rect()
         paint = Paint()
     }
--- app/src/main/java/slawomir/kustra/starysky/utils/MathUtils.kt
+++ app/src/main/java/slawomir/kustra/starysky/utils/MathUtils.kt
@@ -1,4 +1,14 @@
 package slawomir.kustra.starysky.utils
 
-class MathUtils {
+import java.util.*
+
+
+fun getStarSize(screenWidth: Int): Int {
+    val starSize = screenWidth / 15
+    return rand((starSize * 0.7).toInt(), starSize)
+}
+
+fun rand(from: Int, to: Int): Int {
+    val random = Random()
+    return random.nextInt(to - from) + from
 }
