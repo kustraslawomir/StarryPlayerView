e6aa8b507a82ebbff1b295ed6e8fd983a3a5e083
slawomir.kustra <kustraslawomir@gmail.com>
2019-01-03 10:51:17 +0100

    animate all stars at random interval values

--- app/src/main/java/slawomir/kustra/starysky/view/StarsView.kt
+++ app/src/main/java/slawomir/kustra/starysky/view/StarsView.kt
@@ -12,22 +12,36 @@ import android.os.Handler
 import slawomir.kustra.starysky.R
 import slawomir.kustra.starysky.utils.Star
 import slawomir.kustra.starysky.utils.getStars
-import android.view.animation.Animation
-import android.view.animation.DecelerateInterpolator
-import android.view.animation.ScaleAnimation
 import slawomir.kustra.starysky.utils.rangeRandomValue
-
 
 internal class StarsView : FrameLayout {
 
     private var stars: ArrayList<Star> = arrayListOf()
+
     private var starsContainerHeight = 0
     private var starsContainerWidth = 0
-
     private val alphaPaint = Paint()
 
-    private var scaleAnimation =
-        ScaleAnimation(0.7f, 1f, 0.7f, 1f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+    private var intervalHandler = Handler()
+    private var invalidateHandler = Handler()
+
+    private val starsRunnable = object : Runnable {
+        override fun run() {
+            postDelayed(this, 5000)
+            for (i in 0..stars.size) {
+                animateStar()
+            }
+        }
+    }
+
+    private val invalidateRunnable = object : Runnable {
+        override fun run() {
+            postDelayed(this, 16)
+            invalidate()
+        }
+    }
+
+    private var animatingStars = arrayListOf<Int>()
 
     constructor(context: Context) : super(context) {
         init(context)
@@ -62,7 +76,6 @@ internal class StarsView : FrameLayout {
     private fun init(context: Context) {
         starsContainerWidth = context.resources.displayMetrics.widthPixels
         starsContainerHeight = context.resources.displayMetrics.heightPixels
-        println("starsContainerHeight: $starsContainerHeight, starsContainerWidth: $starsContainerWidth")
 
         stars = getStars(starsContainerWidth, starsContainerHeight / 2)
 
@@ -77,38 +90,51 @@ internal class StarsView : FrameLayout {
             stars[i].star = star
         }
 
-        scaleAnimation.duration = 500
-        scaleAnimation.fillAfter = true
-
-        runStarsChooserTimer()
+        runAnimationInterval()
     }
 
-    private fun runStarsChooserTimer() {
-        Handler().apply {
-            val runnable = object : Runnable {
-                override fun run() {
-                    postDelayed(this, 3000)
+    private fun runAnimationInterval() {
 
-                    val starToAnimate = rangeRandomValue(0, stars.size)
-                    val animator = ValueAnimator.ofInt(0, 100)
-
-                    animator.duration = 2000
-                    animator.interpolator = DecelerateInterpolator()
-                    animator.addUpdateListener { animation ->
-
-                        val animatedValue = animation.animatedValue.toString().toInt()
-                        val starAlphaValue = (animatedValue * 0.01 * 255).toInt()
-
-                        if (animatedValue >= 50)
-                            stars[starToAnimate].alpha = starAlphaValue
-                        else
-                            stars[starToAnimate].alpha = 255 - starAlphaValue
-                        invalidate()
-                    }
-                    animator.start()
-                }
-            }
-            postDelayed(runnable, 0)
+        intervalHandler.apply {
+            postDelayed(starsRunnable, 0)
         }
+
+        invalidateHandler.apply {
+            postDelayed(invalidateRunnable, 0)
+        }
+    }
+
+    private fun animateStar() {
+
+        val animator = ValueAnimator.ofInt(0, 100)
+        animator.duration = rangeRandomValue(2000, 5000).toLong()
+
+        var starToAnimate = rangeRandomValue(0, stars.size)
+
+        while (animatingStars.contains(starToAnimate) && animatingStars.size < stars.size) {
+            starToAnimate = rangeRandomValue(0, stars.size)
+        }
+
+        animatingStars.add(starToAnimate)
+
+        animator.addUpdateListener { animation ->
+            val animatedValue = animation.animatedValue.toString().toInt()
+            val starAlphaValue = (animatedValue * 0.01 * 255).toInt()
+
+            if (animatedValue >= 50)
+                stars[starToAnimate].alpha = starAlphaValue
+            else
+                stars[starToAnimate].alpha = 255 - starAlphaValue
+
+            if (animatedValue == 100) {
+                animatingStars.remove(starToAnimate)
+            }
+        }
+        animator.start()
+    }
+
+    internal fun clearHandler() {
+        intervalHandler.removeCallbacks(starsRunnable)
+        invalidateHandler.removeCallbacks(invalidateRunnable)
     }
 }
